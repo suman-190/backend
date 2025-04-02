fa81a726aeebc661d8d859b71d50770cd4b701f0
Suman Bhandari <149918576+suman-190@users.noreply.github.com>
2025-04-02 11:45:14 +0000

    crud operation

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-
+app.use(express.json())
 
 app.use("/users",(req,res)=>{
     res.send({"name":"John Doe", "age": 30})
@@ -14,6 +14,50 @@ app.use("/users",(req,res)=>{
 app.use("/",(req,res)=>{
    res.send("Hello World!")
 })
+
+
+
+
+
+ // Middleware to parse JSON bodies
+
+//create a database
+// 
+
+
+const data = [{
+   id:1,
+   name:"Sumjit",
+   age:23,
+}]
+
+
+app.get("/get-user/:id",(req,res)=>{
+   console.log(req.params.id)
+    res.status(200).send(data)
+ })
+
+
+app.post("/create-user",(req,res)=>{
+   console.log(req.body) 
+    res.status(200).send({status:"success",
+      message:"User created successfully",
+    })
+ })
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
